51575e17cc31d2706fe6ac1f0cfbb8304d6e2914
Rachel Carleton <rachel.carleton@icloud.com>
2024-01-02 10:40:57 -0800

    reviews controller format

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,8 +1,7 @@
 const Book = require('../models/book');
 
 
-async function deleteReview(req, res) {
-  console.log("test")
+const deleteReview = async (req, res) => {
   const book = await Book.findOne({ 'reviews._id': req.params.id, 'reviews.user': req.user._id });
   if (!book) return res.redirect('/books');
   book.reviews.remove(req.params.id);
@@ -10,18 +9,15 @@ async function deleteReview(req, res) {
   res.redirect(`/books/${book._id}`);
 }
 
-async function create(req, res) {
+const create = async (req, res) => {
   const book = await Book.findById(req.params.id);
-
   req.body.user = req.user._id;
   req.body.userName = req.user.name;
   req.body.userAvatar = req.user.avatar;
-
   book.reviews.push(req.body);
   try {
     await book.save();
   } catch (err) {
-    console.log(err);
   }
   res.redirect(`/books/${book._id}`);
 }
